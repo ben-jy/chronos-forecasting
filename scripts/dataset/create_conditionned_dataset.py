eec886bd8d2a5ffbc8887df4a3893ce2e6f46feb
Benjamin Pasquier <benjamin.pasquier@hefr.ch>
2025-08-20 14:50:47 +0200

    add frequency and domain handling in dataset processing. Update dependencies and add sample subsets.

--- scripts/dataset/create_conditionned_dataset.py
+++ scripts/dataset/create_conditionned_dataset.py
@@ -6,39 +6,65 @@ import argparse
 import numpy as np
 import pyarrow as pa
 import json
+from pandas.tseries.frequencies import to_offset
 
-with open("scripts/dataset/subsets.json", "r") as f:
-    subsets = json.load(f)
+DEFAULT_START = np.datetime64("2000-01-01 00:00", "s")
+
+def get_frequency_id(freq: str) -> int:
+    try:
+        offset = to_offset(freq)
+    except ValueError:
+        raise ValueError(f"Invalid frequency string: {freq}")
+
+    rule = offset.rule_code
+
+    high_freq = {"ns", "us", "ms", "s", "min", "h"}
+    mid_freq = {"D", "B", "W", "W-MON", "W-SUN"}
+
+    if rule in high_freq:
+        return 0
+    elif rule in mid_freq:
+        return 1
+    else:
+        return 2
+
+def get_domain_id(domain: str) -> int:
+    if domain == "transport":
+        return 0
+    elif domain == "weather":
+        return 1
+    elif domain == "energy":
+        return 2
+    elif domain == "web":
+        return 3
+    else:
+        return 4 # i.e. any
     
 def split_sequence(seq, max_len):
     seq = np.array(seq)
     # mean scaling
     # mean = np.nanmean(np.abs(seq))
     # seq = seq / mean if mean != 0 else seq
-    if max_len is not None and len(seq) > max_len:
+    # check if seq is only a number
+    if max_len is not None and seq.size > max_len:
         return [seq[i:i + max_len] for i in range(0, len(seq), max_len)]
     else:
         return [seq]
 
-def process_features(features, ndim, max_timesteps=None):
-    series = []
-    if ndim > 1:
-        for f in features:
-            series.extend(split_sequence(f, max_timesteps))
-    else:
-        series.extend(split_sequence(features, max_timesteps))
-    return series
-
-def process_batch(ds_features, batch, max_timesteps=None, target_name='target'):
-    target_ndim = ds_features[target_name].length
-    series = []
-    for entry in batch[target_name]:
-        series.extend(process_features(entry, target_ndim, max_timesteps))
-    if 'past_feat_dynamic_real' in ds_features:
-        past_feat_ndim = ds_features['past_feat_dynamic_real'].length
-        for entry in batch['past_feat_dynamic_real']:
-            series.extend(process_features(entry, past_feat_ndim, max_timesteps))
-    return series
+def process_batch(batch, target, max_timesteps=None, freq=0):
+    subset = []
+    for t in target:
+        series = []
+        target_name = t['name']
+        target_domain_id = get_domain_id(t['domain'])
+        for entry in batch[target_name]:
+            series.extend(split_sequence(entry, max_timesteps))
+        subset.extend([
+            {
+                "start": DEFAULT_START, "target": np.array(ts, dtype=np.float32),
+                "freq": freq, "domain": target_domain_id
+            } for ts in series])
+    return subset
 
 def batched(ds, batch_size):
     for i in range(0, len(ds), batch_size):
@@ -46,16 +72,18 @@ def batched(ds, batch_size):
 
 def main():
     parser = argparse.ArgumentParser(description="Convert a huggingface to arrow files")
+    parser.add_argument("--subset_info_file", type=str, required=True, default="subset.json")
     parser.add_argument("--batch_size", type=int, default=None, help="The batch size to load in memory before writing to the arrow file.")
     parser.add_argument("--max_timesteps", type=int, default=None, help="Maximum number of time steps per series. Longer series will be split.")
-    parser.add_argument("--arrow_output_path", type=str, default="lotsa.arrow", help="Output Arrow file")
+    parser.add_argument("--arrow_output_path", type=str, default="output.arrow", help="Output Arrow file")
     args = parser.parse_args()
+
+    with open(args.subset_info_file, "r") as f:
+        subsets = json.load(f)
 
     writer = None
     output_path = Path(args.arrow_output_path)
     output_path.parent.mkdir(parents=True, exist_ok=True)
-
-    default_start = np.datetime64("2000-01-01 00:00", "s")
 
     for _, subset in subsets.items():
         print(f"Processing subsets {subset['name']}...")
@@ -64,19 +92,17 @@ def main():
         ds.set_format("numpy")
         
         print(f"Keys : {ds[0].keys()}")
-        subset_target_name = subset['target_name']
-        subset_freq = subset['freq']
-        subset_domain = subset['domain']
+        subset_target = subset['target']
+        subset_freq_id = get_frequency_id(subset['freq'])
 
         batch_size = args.batch_size if args.batch_size is not None else len(ds)
         
         for i, batch in enumerate(batched(ds, batch_size)):
             print(f"Processing batch {i + 1}: [{i * batch_size}:{min((i + 1) * batch_size, len(ds))}]")
-            processed_series = process_batch(ds.features, batch, max_timesteps=args.max_timesteps, target_name=subset_target_name)
-            dataset = [{"start": default_start, "target": np.array(ts, dtype=np.float32),
-                        "frequency": subset_freq, "domain": subset_domain} for ts in processed_series]
+            subset_batch = process_batch(batch, max_timesteps=args.max_timesteps,
+                                             target=subset_target, freq=subset_freq_id)
 
-            batches = list(into_arrow_batches(dataset, flatten_arrays=True))
+            batches = list(into_arrow_batches(subset_batch, flatten_arrays=True))
             
             if not batches:
                 continue
