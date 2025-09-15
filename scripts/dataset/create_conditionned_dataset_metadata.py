6fdd87a5f59fa7c3fd681766b0f2836e38a6917c
ben-jy <benjamin.pasquier@bluewin.ch>
2025-09-15 11:12:07 +0000

    update script to generate metadata for conditionned dataset : we do not need to load the dataset entirely anymore, we use streaming instead. create the metadata create a script to run the dataset creation on slurm

--- scripts/dataset/create_conditionned_dataset_metadata.py
+++ scripts/dataset/create_conditionned_dataset_metadata.py
@@ -11,13 +11,13 @@ def main():
         subset.pop("domain", None)
         subset.pop("target_name", None)
 
-        ds = datasets.load_dataset(subset['hf_dataset_name'], subset['hf_subset_name'], keep_in_memory=False, split='train', trust_remote_code=True)
-        ds.set_format("numpy")
+        ds = datasets.load_dataset(subset['hf_dataset_name'], subset['hf_subset_name'], streaming=True, split='train', trust_remote_code=True)
+        sample = next(iter(ds))
 
         subset['target'] = []
 
-        for col in ds.column_names:
-            if col not in ["timestamp", "date", "time"] and ds[col].dtype == np.ndarray:
+        for col in sample.keys():
+            if col not in ["timestamp", "date", "time"] and isinstance(sample[col], list):
                 subset['target'].append({
                     "name": col,
                     "domain": "#TODO"
