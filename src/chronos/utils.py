4066305c89b70d939aa8759f2844afe08dd54593
ben-jy <benjamin.pasquier@bluewin.ch>
2025-09-17 14:59:00 +0000

    move frequency and domain id getter to utils module

--- src/chronos/utils.py
+++ src/chronos/utils.py
@@ -3,7 +3,7 @@
 
 
 from typing import List
-
+from pandas.tseries.frequencies import to_offset
 import torch
 
 
@@ -18,3 +18,34 @@ def left_pad_and_stack_1D(tensors: List[torch.Tensor]) -> torch.Tensor:
         )
         padded.append(torch.concat((padding, c), dim=-1))
     return torch.stack(padded)
+
+def get_frequency_id(freq: str) -> int:
+    try:
+        offset = to_offset(freq)
+    except ValueError:
+        raise ValueError(f"Invalid frequency string: {freq}")
+
+    rule = offset.rule_code
+
+    high_freq = {"ns", "us", "ms", "s", "min", "h"}
+    mid_freq = {"D", "B", "W", "W-MON", "W-SUN"}
+
+    if rule in high_freq:
+        return 0
+    elif rule in mid_freq:
+        return 1
+    else:
+        return 2
+
+def get_domain_id(domain: str) -> int:
+    if domain == "transport":
+        return 0
+    elif domain == "weather":
+        return 1
+    elif domain == "energy":
+        return 2
+    elif domain == "web":
+        return 3
+    else:
+        # basically means any
+        return None
